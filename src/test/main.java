08d2b1dac3fe456cc83227fd21cac0f4a8c5dd68
Hardik Hajela <69126651+HardikHajela@users.noreply.github.com>
2023-01-05 04:55:43 -0500

    Update main.java

--- src/test/main.java
+++ src/test/main.java
@@ -2,6 +2,7 @@ package test;
 
 public class main {
 	public static void main(String [] args) {
+		System.out.println("Hello World 0");
 		System.out.println("Hello World 1");
 		System.out.println("Hello World 2");
 		System.out.println("Hello World 3");
